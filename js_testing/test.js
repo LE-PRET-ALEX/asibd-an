c13ef48fd5c90bc2b52c5245da14ff92f37b0249
LE-PRET-ALEX <nascimentoalexcv@gmail.com>
2026-02-13 17:47:10 -0100

    Pagina Dinamicas v1

--- js_testing/test.js
+++ js_testing/test.js
@@ -1,27 +1,8 @@
-/*let nome = "Alexandre";
-console.log(typeof(nome));
+message = "A carroca do Jose esta a passar";
 
-let num = 10;
-console.log(typeof(num));
-
-let status = false;
-console.log(typeof(status));*/
-
-let a = prompt("Insira um numero:");
-let b = prompt("Insira outro numero:");
-let c = prompt("Insira mais um numero:");
-
-if(a>b){
-    if(b>c){
-        console.log(`O numero ${a} e o maior`);
-    }
-    else if(c>a){
-        console.log(`O numero ${c} e o maior`);
-        }
-        else {
-            console.log(`O numero ${a} e o maior`);
-        }
+function showAlert(){
+    let message = "O carro do ovo esta a passar";
+    alert(message);
 }
-else {
 
-}
+showAlert();
